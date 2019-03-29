937e84d076f305d3f79f4c2947d7470f176c6c74
xczhang <xczhang@XCZHANG-CN.jp.oracle.com>
2019-03-29 10:29:14 +0800

    init

---
+++ src/main/java/com/jibtutorial/JibtutorialApplication.java
@@ -0,0 +1,13 @@
+package com.jibtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibtutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibtutorialApplication.class, args);
+	}
+
+}
